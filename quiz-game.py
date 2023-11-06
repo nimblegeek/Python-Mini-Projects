e15417e6b72901608b7c70db7d7b78ce33615c50
Jonas A Sihlén <jonasasihlen@gmail.com>
2023-11-06 22:02:28 +0100

    Updating with scores and lower function

--- quiz-game.py
+++ quiz-game.py
@@ -2,13 +2,32 @@ print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Okay! Let's play :)")
+score = 0
 
-answer = input("What does CPU stand for? ")
+answer = input("What does CPU stand for? ").lower()
 if answer == "central processing unit":
     print("Correct!")
+    score += 1
+
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ").lower()
+if answer == "graphic processing unit":
+    print("Correct!")
+    score += 1
+
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ").lower()
+if answer == "random access memory":
+    print("Correct!")
+    score += 1
+
 else:
     print("Incorrect!")
